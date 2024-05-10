bed3be55038a0d5ba7a20a0ab7292af32c675801
Vlad Verba <vv2298@columbia.edu>
2024-05-10 14:43:11 -0400

    new quiz

---
+++ data.js
@@ -0,0 +1,12 @@
+const questions = [
+  { answer: "Dallas Cowboys", hint: "$8.12 Billion" },
+  { answer: "New England Patriots", hint: "$6.40 Billion" },
+  { answer: "Los Angeles Rams", hint: "$6.20 Billion" },
+  { answer: "New York Yankees", hint: "$6.00 Billion" },
+  { answer: "New York Giants", hint: "$6.00 Billion" },
+  { answer: "New York Knicks", hint: "$5.80 Billion" },
+  { answer: "Chicago Bears", hint: "$5.60 Billion" },
+  { answer: "Golden State Warriors", hint: "$6.20 Billion" },
+  { answer: "Washington Commanders", hint: "$5.60 Billion" },
+  { answer: "Los Angeles Lakers", hint: "$5.50 Billion" },
+];
---
+++ script.js
@@ -0,0 +1,121 @@
+let interval;
+let score = 0;
+const answered = [];
+
+questions.forEach((question, index) => {
+  question.rowIndex = index;
+});
+
+document.getElementById("max-score").textContent = questions.length;
+
+populateHints();
+
+function populateHints() {
+  const tableBody = document.getElementById("correct-answers");
+  questions.forEach((question) => {
+    const row = tableBody.insertRow(-1);
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    cell1.innerHTML = '<span class="answer"></span>';
+    cell2.textContent = question.hint;
+  });
+}
+
+function startQuiz() {
+  var startButton = document.getElementById("start-button");
+  var userInput = document.getElementById("answer-input");
+
+  startButton.classList.add("d-none");
+  userInput.classList.remove("d-none");
+  userInput.focus(); // Automatically focus on the text field
+
+  // Additional actions specific to starting the quiz can be added here
+  interval = setInterval(countdown, 1000); // Start countdown timer, etc.
+}
+
+// Listen for input and check if answer is correct
+document.getElementById("answer-input").addEventListener("input", checkAnswer);
+
+// Listen for enter key and check if answer is correct
+document
+  .getElementById("answer-input")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      checkAnswer();
+    }
+  });
+
+function checkAnswer() {
+  const userInput = document.getElementById("answer-input").value;
+  const inputBox = document.getElementById("answer-input");
+  const lowerCaseUserInput = userInput.toLowerCase();
+
+  const question = questions.find((q) => {
+    const answerWords = q.answer.split(" ");
+    const lastWord = answerWords[answerWords.length - 1].toLowerCase();
+    return (
+      q.answer.toLowerCase() === lowerCaseUserInput ||
+      lastWord === lowerCaseUserInput
+    );
+  });
+
+  if (question && !answered.includes(question.answer)) {
+    inputBox.style.backgroundColor = "#90EE90"; // change border color to green
+
+    setTimeout(() => {
+      inputBox.style.backgroundColor = "white"; // revert to original color after 0.5 seconds
+    }, 500);
+
+    answered.push(question.answer); // Store the correct case answer
+    score++;
+    document.getElementById("current-score").textContent = score;
+
+    // Populate the answer directly based on rowIndex
+    document.querySelectorAll(".answer")[question.rowIndex].textContent =
+      question.answer;
+
+    if (score === questions.length) {
+      endQuiz("won");
+    }
+
+    document.getElementById("answer-input").value = "";
+  }
+}
+
+function countdown() {
+  const timeElement = document.getElementById("time-left");
+  const currentTime = parseInt(timeElement.textContent);
+  if (currentTime > 0) {
+    timeElement.textContent = currentTime - 1;
+  } else {
+    endQuiz("timeout");
+  }
+}
+
+function endQuiz(reason) {
+  clearInterval(interval);
+  document.getElementById("answer-input").disabled = true;
+  document.getElementById("submit-button").disabled = true;
+  document.getElementById("give-up").disabled = true;
+
+  // Populate the table with answers not filled in
+  const tableRows = document.querySelectorAll(".answer");
+  for (let i = 0; i < tableRows.length; i++) {
+    if (tableRows[i].textContent === "") {
+      tableRows[i].textContent = questions[i].answer;
+      tableRows[i].style.color = "red";
+    }
+  }
+
+  let message = `Your score is ${score}/${questions.length}.`;
+
+  setTimeout(() => {
+    if (reason === "timeout") {
+      alert(`Time's up! ${message}`);
+    } else if (reason === "won") {
+      alert(`You won! ${message}`);
+    } else if (reason === "giveup") {
+      alert(`You gave up. ${message}`);
+    }
+  }, 100); // Delay alert by 100ms to give DOM time to update
+}
